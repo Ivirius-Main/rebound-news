e0e00625f1c69429217ae6fb509b10945d17c9fa
jpb <jakub.baranski.33@gmail.com>
2024-03-15 21:45:00 +0000

    fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,13 +16,14 @@
 
 <div class="flex flex-col flex-grow gap-8 pb-16">
   <!-- bio -->
+  <section class="flex flex-col items-center gap-16 pt-8 pb-16">
       <div class="flex gap-6">
 		  <Fluent.Button variant="accent" onclick="window.location.href='https://github.com/jpbandroid';">My GitHub</Fluent.Button>
 	  </div>
       <p class="text-base text-zinc-600 dark:text-zinc-400">
         {bio}
       </p>
-    </div>
+  </section>
   <section class="w-full">
     <div class="flex items-center justify-between gap-4 mb-8">
       <h2 class="text-sm font-medium sm:text-base text-zinc-500 dark:text-zinc-400">
