212b8cb6255e841af2499424b1911a7282d0e8a0
jpb <jakub.baranski.33@gmail.com>
2024-03-15 20:37:10 +0000

    Modify branding

--- src/lib/info.js
+++ src/lib/info.js
@@ -5,23 +5,19 @@
 // URL of your website
 export const website = 'https://sveltekit-blog-template.vercel.app'
 
-export const firstName = 'Jordan'
-export const lastName = 'Doe'
+export const firstName = 'ErrorTek\'s'
+export const lastName = 'Blog'
 export const name = `${firstName} ${lastName}`
 
 // Used for the landing page & footer of posts
-export const avatar = 'https://picsum.photos/id/203/250/250'
+export const avatar = ''
 export const bio = `\
-I’m ${firstName}, a software engineer based in Canada. Lorem ipsum dolor sit amet, consectetur
-adipiscing elit. Phasellus accumsan quam at eros malesuada, id luctus lectus efficitur. Nulla
-sit amet arcu mi. Duis quis mauris malesuada, posuere orci et, pulvinar nisl. Nam malesuada
-vehicula sapien vitae sagittis.
-`
+Welcome to ${firstName}! On this site, I will post updates about my software projects, YouTube channel, and more!`
 
 // Your social usernames, empty strings won't be used.
 // For sake of the live demo, I'm using `username/repo` for github,
 // but you can replace with just your username.
-export const github = 'mattjennings/sveltekit-blog-template'
+export const github = 'jpbandroid/website-2'
 export const twitter = ''
 export const linkedin = ''
 export const instagram = ''
