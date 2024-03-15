12c3438c577e01133bfe01454ac56499b3ba153e
jpb <jakub.baranski.33@gmail.com>
2024-03-15 21:36:22 +0000

    Update some text, fluentize

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,17 @@
 <script>
-  import '../app.css'
-  import '../prism.css'
-  import MoonIcon from 'heroicons-svelte/solid/MoonIcon.svelte'
-  import SunIcon from 'heroicons-svelte/solid/SunIcon.svelte'
-  import { browser } from '$app/environment'
-  import { name } from '$lib/info'
-  import { page } from '$app/stores'
+	import '../app.css'
+	import '../prism.css'
+	import MoonIcon from 'heroicons-svelte/solid/MoonIcon.svelte'
+	import SunIcon from 'heroicons-svelte/solid/SunIcon.svelte'
+	import { browser } from '$app/environment'
+	import { name } from '$lib/info'
+	import { page } from '$app/stores'
+	import { Button, Checkbox } from "fluent-svelte";
 
-  let isDarkMode = browser ? Boolean(document.documentElement.classList.contains('dark')) : true
+	let isDarkMode = browser ? Boolean(document.documentElement.classList.contains('dark')) : true
 
-  function disableTransitionsTemporarily() {
-    document.documentElement.classList.add('[&_*]:!transition-none')
+	function disableTransitionsTemporarily() {
+	document.documentElement.classList.add('[&_*]:!transition-none')
     window.setTimeout(() => {
       document.documentElement.classList.remove('[&_*]:!transition-none')
     }, 0)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
-  import ArrowRightIcon from '$lib/components/ArrowRightIcon.svelte'
-  import PostsList from '$lib/components/PostsList.svelte'
-  import SocialLinks from '$lib/components/SocialLinks.svelte'
-  import { avatar, bio, name } from '$lib/info.js'
+	import ArrowRightIcon from '$lib/components/ArrowRightIcon.svelte'
+	import PostsList from '$lib/components/PostsList.svelte'
+	import SocialLinks from '$lib/components/SocialLinks.svelte'
+	import { avatar, bio, name } from '$lib/info.js'
+	import { Button, Checkbox } from "fluent-svelte";
 
-  /** @type {import('./$types').PageData} */
-  export let data
+	/** @type {import('./$types').PageData} */
+	export let data
 </script>
 
 <svelte:head>
@@ -16,15 +17,9 @@
 <div class="flex flex-col flex-grow gap-8 pb-16">
   <!-- bio -->
   <section class="flex flex-col items-center gap-16 pt-8 pb-16">
-    <div class="flex flex-col items-center w-full gap-6 rounded-lg">
-      <img
-        src={avatar}
-        alt={name}
-        class="mx-auto rounded-full w-36 h-36 ring-2 ring-zinc-200 dark:ring-zinc-700"
-      />
       <div class="flex gap-6">
-        <SocialLinks />
-      </div>
+		  <Fluent.Button variant="accent" onclick="window.location.href='https://github.com/jpbandroid';">My GitHub</Fluent.Button>
+	  </div>
       <p class="text-base text-zinc-600 dark:text-zinc-400">
         {bio}
       </p>
@@ -33,7 +28,7 @@
   <section class="w-full">
     <div class="flex items-center justify-between gap-4 mb-8">
       <h2 class="text-sm font-medium sm:text-base text-zinc-500 dark:text-zinc-400">
-        Recently Published
+        Recently Published Posts
       </h2>
       <a href="/posts" class="flex items-center gap-1 text-sm font-medium text-teal-500"
         >View All <ArrowRightIcon class="w-4 h-4" /></a
