fd326922216428393e89236fafb48aee197bc845
jpb <jakub.baranski.33@gmail.com>
2024-03-16 16:53:48 +0000

    fluentize part 3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,43 +19,42 @@
 </script>
 
 <div class="flex flex-col min-h-screen ">
-  <div class="flex flex-col flex-grow w-full px-4 py-2">
-    <header class="flex items-center justify-between w-full max-w-2xl py-4 mx-auto lg:pb-8">
-      <a
-        class="text-lg font-bold sm:text-2xl !text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-teal-600 dark:to-teal-400"
-        href="/"
-      >
-        {name}
-      </a>
+	<div class="flex flex-col flex-grow w-full px-4 py-2">
+		<Fluent.TextBlock variant="titleLarge">
+			<a href="/">
+				{name}
+			</a>
+		</Fluent.TextBlock>
 
-      <button
-        type="button"
-        role="switch"
-        aria-label="Toggle Dark Mode"
-        aria-checked={isDarkMode}
-        class="w-5 h-5 sm:h-8 sm:w-8 sm:p-1"
-        on:click={() => {
-          isDarkMode = !isDarkMode
-          localStorage.setItem('isDarkMode', isDarkMode.toString())
 
-          disableTransitionsTemporarily()
+		<button
+		  type="button"
+		  role="switch"
+		  aria-label="Toggle Dark Mode"
+		  aria-checked={isDarkMode}
+		  class="w-5 h-5 sm:h-8 sm:w-8 sm:p-1"
+		  on:click={() =>
+			{
+			isDarkMode = !isDarkMode
+			localStorage.setItem('isDarkMode', isDarkMode.toString())
 
-          if (isDarkMode) {
-            document.querySelector('html').classList.add('dark')
-          } else {
-            document.querySelector('html').classList.remove('dark')
-          }
-        }}
-      >
-        <MoonIcon class="hidden text-zinc-500 dark:block" />
-        <SunIcon class="block text-zinc-400 dark:hidden" />
-      </button>
-    </header>
-    <main
-      class="flex flex-col flex-grow w-full mx-auto"
-      class:max-w-2xl={!$page.data.layout?.fullWidth}
+			disableTransitionsTemporarily()
+
+			if (isDarkMode) {
+			document.querySelector('html').classList.add('dark')
+			} else {
+			document.querySelector('html').classList.remove('dark')
+			}
+			}}>
+			<MoonIcon class="hidden text-zinc-500 dark:block" />
+			<SunIcon class="block text-zinc-400 dark:hidden" />
+		</button>
+		
+		<main
+		  class="flex flex-col flex-grow w-full mx-auto"
+		  class:max-w-2xl={!$page.data.layout?.fullWidth}
     >
-      <slot />
-    </main>
-  </div>
+			<slot />
+		</main>
+	</div>
 </div>
