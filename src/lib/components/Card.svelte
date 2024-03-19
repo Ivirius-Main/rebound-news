eb7a5d613d73168774a9c3e23b8c2480193b7651
jpb <jakub.baranski.33@gmail.com>
2024-03-19 07:13:05 +0000

    Revert "Add Fluent Svelte styles to Card"
    This reverts commit 6c961951a9d0b26b940c40112f6c6bf572be9623.

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,14 +1,9 @@
 <script>
-	export let as = 'div'
-	export let href = undefined
+  export let as = 'div'
+  export let href = undefined
 
-	let _class = undefined
-	export { _class as class }
-
-
-	import * as Fluent from "fluent-svelte";
-	import "fluent-svelte/theme.css";
-	import { Button, Checkbox, TextBlock } from "fluent-svelte";
+  let _class = undefined
+  export { _class as class }
 </script>
 
 <svelte:element this={as} class={['relative flex flex-col items-start group', _class].join(' ')}>
@@ -17,7 +12,8 @@
   {#if $$slots.title}
     <div class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
       {#if href}
-        <div class="blogCard"
+        <div
+          class="absolute z-0 transition scale-95 opacity-0 -inset-y-6 -inset-x-4 bg-zinc-50 group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"
         />
         <a {href} data-sveltekit-prefetch>
           <span class="absolute z-20 -inset-y-6 -inset-x-4 sm:-inset-x-6 sm:rounded-2xl" />
@@ -46,11 +42,3 @@
     </div>
   {/if}
 </svelte:element>
-
-<style>
-	.blogCard {
-	border: 1px solid var(--fds-card-stroke-default);
-	border-radius: var(--fds-overlay-corner-radius);
-	background-color: var(--fds-card-background-secondary);
-	}
-</style>
